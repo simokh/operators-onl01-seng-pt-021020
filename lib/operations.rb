39cddc678e9f5f0652babe725bfdca96e93c527a
Simo Khouili <m.simokhouili@gmail.com>
2020-02-14 05:13:40 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -5,7 +5,7 @@ def unsafe?(speed)
 		true
 	elsif speed < 40
 		true
-	elsif speed>= 40 && speed <=60 
+	elsif speed>= 40 && speed <=60
 		false
 	end
 end
@@ -13,15 +13,15 @@ end
 
 
 def not_safe?(speed)
-	if speed > 60 
-		true 
-	elsif speed <40 
-		true 
-	elsif speed>= 40 && speed <=60 
+	if speed > 60
+		true
+	elsif speed <40
+		true
+	elsif speed>= 40 && speed <=60
 		false
-	end 
+	end
 end 
-		
-		
+
+
 
 end
