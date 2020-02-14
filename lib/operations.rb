113cbd4dd20e389ffa29b558dfd03212ccd06beb
Simo Khouili <m.simokhouili@gmail.com>
2020-02-14 04:58:00 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
-require 'pry'
+# require 'pry'
 def unsafe?(speed)
-	binding.pry
+	# binding.pry
 	if speed > 60
 		true
 	elsif speed < 35
 		true
-	elsif speed == (40..60)
-		false
+	# elsif speed == (40..60)
+	# 	false
 	end
 end
 
